140899d2a1bec2c1253987205028bfbf792fd8fa
oskar0kh <oskar0kh@g.skku.edu>
2025-07-13 17:32:52 +0900

    [POST], [DELETE] 구현

--- backend/route.js
+++ backend/route.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import fs from 'fs';
+import fs, { write } from 'fs';
 import path from 'path';
 
 const router = express.Router();
@@ -12,13 +12,14 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 ////
 
+// [GET] 전체 json 데이터 가져오기
 router.get('/', (req, res) => {
     const filePath = path.join(__dirname, 'data', 'restaurants.json'); // /backend/data/resturants.json으로 이동
 
-    fs.readFile(filePath, 'utf-8', (err, data) => {             // 1. 파일 경로 (json 파일) 읽기
+    fs.readFile(filePath, 'utf-8', (err, data) => {                  // 1. 파일 경로 (json 파일) 읽기
         if(err){
             console.error('파일 읽기 오류: ${err}');
-            return res.status(500).json({ error : '서버 오류'}); // 2. return으로 아래 코드 실행되는거 막기
+            return res.status(500).json({ error : '파일 읽기 오류'}); // 2. return으로 아래 코드 실행되는거 막기
         }
 
         // 위에서 읽어온 json data 파싱 -> Client에게 전달
@@ -37,7 +38,7 @@ router.get('/', (req, res) => {
 });
 
 
-// 'name'을 통해 특정 restaurant 정보 출력 (Query, Parameter, Body 사용)
+// [GET] 'name'을 통해 특정 restaurant 정보 출력 (Query, Parameter 사용)
 
 // Query 형식
 // URL : http://localhost:3000/find?name=먹거리 고을
@@ -52,8 +53,8 @@ router.get('/find', (req, res) => {
     fs.readFile(filePath, 'utf-8', (err, data) => {
 
         if(err){
-            console.log('파일 읽기 오류 : ${err}');
-            return res.status(500).json({ error : '서버 오류'});
+            console.error('파일 읽기 오류 : ${err}');
+            return res.status(500).json({ error : '파일 읽기 오류'});
         }
 
         try{
@@ -76,6 +77,121 @@ router.get('/find', (req, res) => {
         } catch(parseErr){
             console.error('JSON 파싱 오류: ', parseErr);
             res.status(500).json({ error : 'JSON 파싱 실패'});
+        }
+
+    });
+});
+
+// [POST] restaurants.json에 restaurants 데이터 추가하기
+router.post('/', (req, res) => {
+    const filePath = path.join(__dirname, 'data', 'restaurants.json');
+
+    const newRestaurant = req.body; // 새로운 restaurant 배열 생성
+
+    // 2. 기존 json파일에서, 새로운 restaurant이랑 이름 같은거 있는지 확인
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+       
+        if(err){
+            console.error(`파일 읽기 오류 : ${err}`);
+            return res.status(500).json({ error : '파일 읽기 오류'});
+        }
+
+        try {
+
+            // 2-1. 전체 json 데이터 가져오기
+            const entire = JSON.parse(data);
+
+            // 2-2. 전체 json 데이터 중 restaurants 배열 가져오기
+            const restaurantList = entire.restaurants; // 배열 생성
+
+            // 2-3. 기존 restaurant의 name이랑 일치하는 새로운 name 있는지 확인
+            const result = restaurantList.find(r => r.name === newRestaurant.name);
+            
+            // 2-4. 만약 기존 name이랑 같은게 있으면, error 반환
+            if(result) return res.status(400).json({error : '이미 해당 맛집 정보가 존재합니다'});
+
+            // 2-5. 만약 기존 name이랑 같은거 없으면, restaurants 배열에 새로운 restaurant 정보 추가
+            restaurantList.push(newRestaurant);
+
+            // 2-6. 수정된 restaurant 배열을 다시 JSON으로 저장
+            const updated = JSON.stringify({restaurants : restaurantList}, null, 2);
+
+            fs.writeFile(filePath, updated, 'utf-8', (writeErr) => {
+            
+                if(writeErr){
+                    console.error(`파일 쓰기 오류 : ${writeErr}`);
+                    return res.status(500).json({error : '파일 저장 실패'});
+                }
+
+                // json 저장 성공
+                res.status(201).json(newRestaurant);
+            
+            });
+
+        } catch(parseErr){
+
+            console.error(`JSON 파싱 오류 : ${parseErr}`);
+            res.status(500).json({error : 'JSON 파싱 실패'});
+
+        }
+
+    });
+});
+
+// [DELETE] 특정 name의 데이터 삭제
+// Query 방식 사용
+// URL : http://localhost:3000/delete?name=먹거리 고을
+
+router.delete('/delete', (req, res) => {
+    const filePath = path.join(__dirname, 'data', 'restaurants.json');
+
+    const name = req.query.name; // 1. name 담은 query 생성
+
+    fs.readFile(filePath, 'utf-8', (err, data) => {
+
+        if(err){
+            console.error(`파일 읽기 오류 : ${err}`);
+            return res.status(500).json({error : '파일 읽기 오류'});
+        }
+
+        try{
+
+            // 2. 전체 json 파일 가져오기
+            const entire = JSON.parse(data);
+
+            // 3. 그 중, restaurants 배열만 가져오기
+            const restaurantList = entire.restaurants;
+
+            // 4. 해당 배열에서 name이 같은거 있는지 찾기 (index 받아오기)
+            const index = restaurantList.findIndex(r => r.name === name);
+
+            // 5. index 못찾았을 때, error 반환
+            if(index === -1) {
+                return res.status(404).json({ error: '해당 맛집 정보가 존재하지 않습니다' });
+            }
+
+            // 6 해당 index의 데이터 삭제하기 (splice) -> removed array에 저장
+            const removed = restaurantList.splice(index, 1);
+            const removedItem = removed[0];
+
+            // 7. 수정된 restaurantList를 다시 json 파일에 저장
+            const updated = JSON.stringify({restaurants : restaurantList}, null, 2);
+
+            fs.writeFile(filePath, updated, 'utf-8', (writeErr) => {
+
+                if(writeErr){
+                    console.error(`파일 쓰기 오류 : ${writeErr}`);
+                    return res.status(500).json({error : '파일 저장 실패'});
+                }
+
+                // 7-1. json 저장 성공 -> 삭제된 데이터 저장한거 반환하기
+                res.status(201).json(removedItem);
+
+            });
+
+        } catch(parseErr){
+            console.error(`JSON 파싱 오류 : ${parseErr}`);
+            res.status(500).json({error : 'JSON 파싱 실패'});
         }
 
     });
--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,9 @@ import route from './route.js';
 
 const app = express();
 
-// '/' 링크로 들어오는 HTTP 요청들은, 다 route.js로 넘어가서 처리됨
+// 1. request 속 JSON을 req.body에 parsing해서 넣어줌
+app.use(express.json());
+// 2. parsing 끝나면, '/' 링크로 들어오는 HTTP 요청들을 다 route.js로 넘어가서 처리
 app.use('/', route);
 
 const PORT = 3000;
